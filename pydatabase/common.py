5dd286d7836a22d675f6d49db9bd7a7ee2edd8ea
mrafea <mohamed.rafea@gmail.com>
2019-02-21 12:51:15 +0200

    added new sql operator

--- pydatabase/common.py
+++ pydatabase/common.py
@@ -20,5 +20,32 @@ class Common(object):
         for k in sorted(orig.__dict__.keys()):
             if k[0] != '_' and k not in excludedFields:
                 self.__dict__[k] = orig.__dict__[k]
+    @classmethod
+    def toCSV(self,objects,fileName):
+        out = open(fileName, 'w')
+        out.write(objects[0].rowHeader())
+        for o in objects:
+            out.write(o.rowValues())
+        out.close()
+    def rowHeader(self):
+        h = ''
+        for k in sorted(self.__dict__.keys()):
+            if k[0] != '_':
+                if len(h)==0:
+                    h = k
+                else:
+                    h = h + ','+k
+        return h+'\n'
+
+    def rowValues(self):
+        r = ''
+        for k in sorted(self.__dict__.keys()):
+            v = str(self.__dict__[k])
+            if k[0] != '_':
+                if len(r)==0:
+                    r = v
+                else:
+                    r = r + ','+v
+        return r+'\n'
 
 
--- pydatabase/table/sqloperator.py
+++ pydatabase/table/sqloperator.py
@@ -6,8 +6,10 @@ class SQLOperator(Common):
     GREATER_THAN = '>'
     GREATER_THAN_OR_EQUAL = '>='
     IN = 'in'
+    NOT_IN = 'not in'
     OR = ''
     NOT_EQUAL = '!='
+    ARRAY_CONTAINS = '@>'
     @classmethod
     def init(cls):
         cls.equalOperator = SQLOperator(cls.EQUAL)
@@ -16,7 +18,9 @@ class SQLOperator(Common):
         cls.greaterThanOperator = SQLOperator(cls.GREATER_THAN)
         cls.greaterThanOrEqualOperator = SQLOperator(cls.GREATER_THAN_OR_EQUAL)
         cls.inOperator = SQLOperator(cls.IN)
+        cls.notInOperator = SQLOperator(cls.NOT_IN)
         cls.notEqual = SQLOperator(cls.NOT_EQUAL)
+        cls.arreyContainsOperator = SQLOperator(cls.ARRAY_CONTAINS)
 
     def __init__(self,op,field=None,value=None):
         self.operator = op
@@ -35,8 +39,12 @@ class SQLOperator(Common):
             return (self.field>=self.value)
         if self.operator==SQLOperator.IN:
             return (self.field.in_(self.value))
+        if self.operator==SQLOperator.NOT_IN:
+            return (~self.field.in_(self.value))
         if self.operator==SQLOperator.NOT_EQUAL:
             return (self.field!=self.value)
+        if self.operator==SQLOperator.ARRAY_CONTAINS:
+            return (self.field.any(self.value))
         raise ValueError("Invalid operator")
     def filter(self,query):
         return query.filter(self.comparisonExpression())
